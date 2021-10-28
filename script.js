b915d574f7c5479b635b567730d1b29a12a284da
Aspas <aspasdev@gmail.com>
2021-10-28 18:10:56 +0300

    Update UI and change play round logic

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-// start game - press button
+// start game
 // define score, p1=0, p2=0
 // 
 // choose player option = start round 
-// generate computer option
-// compare options 
-// determine winner | res = p1,p2,x
-// increase winner's score 
-// repeat until one's score reach 5
-// suggest to restart game
+// round 
+//  generate computer option
+//  show options
+//  compare options 
+//  determine winner | res = p1,p2,x
+//  increase winner's score 
+//  refresh scores
+// check next round possibility
 
-function computerPlay() {
-    const gameValues = ['rock', 'paper', 'scissors'];
-    return gameValues[Math.round(Math.random()*2)];
-}
+const playerSelections = document.querySelectorAll('.player-choice');
+const playerScoreboard = document.querySelector('.player-score');
+const computerScoreboard = document.querySelector('.computer-score');
+const roundResult = document.querySelector('.round-result');
+const playerPick = document.querySelector('.player-selection');
+const computerPick = document.querySelector('.computer-selection');
 
-function playRound(playerSelection) {
-
-    const computerSelection = computerPlay();
-    const gameResult = document.querySelector('.game-result');
-    gameResult.textContent = `Your selection: ${playerSelection} | Computer selection: ${computerSelection}`;
-
-    if (playerSelection === computerSelection) {
-        return `DRAW!`;
-    }
-
-    if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return 'player';
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return 'player';
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return 'player';
-    } else {
-        return 'computer';
-    }
-}
+let playerScore = 0;
+let computerScore = 0;
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-    const playerSelections = document.querySelectorAll('.player-choice');
-    
-    const playerScoreboard = document.querySelector('.player-score');
-    const computerScoreboard = document.querySelector('.computer-score');
-
     playerSelections.forEach(selection => selection.addEventListener('click', () => {
-        let winner = playRound(selection.id);
-        
-        if (winner === 'player') playerScore++;
-            else if (winner === 'computer') computerScore++;
-        playerScoreboard.textContent = playerScore;
-        computerScoreboard.textContent = computerScore;
-    }));
+        playRound(selection.id);
+    }));    
 }
 
 game();
+
+function playRound(playerSelection) {
+    const computerSelection = computerPlay();
+    playerPick.textContent = playerSelection;
+    computerPick.textContent = computerSelection;
+
+    if (playerSelection === computerSelection) {
+        roundResult.textContent = 'IT\'S A TIE';
+        return;
+    }
+
+    if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        roundResult.textContent = 'YOU WIN';
+        playerScore++;
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        roundResult.textContent = 'YOU WIN';
+        playerScore++;
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        roundResult.textContent = 'YOU WIN';
+        playerScore++;
+    } else {
+        roundResult.textContent = 'COMP WINS';
+        computerScore++;
+    }
+
+    playerScoreboard.textContent = playerScore;
+    computerScoreboard.textContent = computerScore;
+}
+
+function computerPlay() {
+    const gameValues = ['rock', 'paper', 'scissors'];
+    return gameValues[Math.round(Math.random() * 2)];
+}
